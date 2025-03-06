ee389550e0ca088d15a7987fb522386d98cd1cd8
Reece Harris <reece@codegalaxy.co.uk>
2025-03-06 16:21:16 +0000

    Add WebAssembly support for WebSocket frame handling

---
+++ server/wasm/handle_websocket_frame/handle_websocket_frame_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const handle_websocket_frame: (a: number, b: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
